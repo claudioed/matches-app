2e7c489e3e0848d02ab640752683d98cdddff341
claudioed <claudioed.oliveira@gmail.com>
2021-04-27 08:09:47 -0300

    logger

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
-module championships
+module matches
 
 go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/motemen/go-loghttp v0.0.0-20170804080138-974ac5ceac27
+	github.com/motemen/go-nuts v0.0.0-20200601065735-3df31f16cb2f // indirect
+	github.com/rs/zerolog v1.21.0
 	github.com/valyala/fasttemplate v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20191105034135-c7e5f84aec59 // indirect
 )
--- main.go
+++ main.go
@@ -3,13 +3,45 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/rs/zerolog"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
+
+var log *zerolog.Logger
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	logger := zerolog.New(output).With().Timestamp().Caller().Logger()
+	log = &logger
+}
 
 func main() {
 	e := echo.New()
 	// Middleware
-	e.Use(middleware.Logger())
+	e.Logger.SetOutput(ioutil.Discard)
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			res := c.Response()
+			start := time.Now()
+			log.Debug().
+				Interface("headers", req.Header).
+				Msg(">>> " + req.Method + " " + req.RequestURI)
+			if err = next(c); err != nil {
+				c.Error(err)
+			}
+			log.Debug().
+				Str("latency", time.Now().Sub(start).String()).
+				Int("status", res.Status).
+				Interface("headers", res.Header()).
+				Msg("<<< " + req.Method + " " + req.RequestURI)
+			return
+		}
+	})
 	e.Use(middleware.Recover())
 	//CORS
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -34,18 +66,16 @@ type HealthData struct {
 }
 
 func GetMatch(c echo.Context) error {
-	m := &MessageError{
-		Message: "Match not found",
+	m := &Match{
+		HomeTeam:     "Barcelona",
+		AwayTeam:     "Real Madrid",
+		Championship: "UEFA",
 	}
-	return c.JSON(http.StatusNotFound, m)
+	return c.JSON(http.StatusOK, m)
 }
 
 type Match struct {
 	HomeTeam     string `json:"homeTeam,omitempty"`
 	AwayTeam     string `json:"awayTeam,omitempty"`
 	Championship string `json:"championship,omitempty"`
-}
-
-type MessageError struct {
-	Message string `json:"message,omitempty"`
 }
